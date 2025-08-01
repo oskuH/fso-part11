9ceb88ac83357bf5e0ec46a6660aa5322cd187f3
Oskari <104070288+oskuH@users.noreply.github.com>
2025-08-01 16:07:08 +0300

    Further troubleshooting

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,13 +1,13 @@
 /* eslint no-console: ["error", { allow: ["log", "error"] }] */
 
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV !== 'tst') {
     console.log(...params);
   }
 };
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV !== 'tst') {
     console.error(...params);
   }
 };
